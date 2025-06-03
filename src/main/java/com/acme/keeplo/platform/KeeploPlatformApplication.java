204354d15028bedec5d9228a9106a5ee2790a358
yigabriel <gabrielsacc7@gmail.com>
2025-06-02 21:23:28 -0500

    initial commit

---
+++ src/main/java/com/acme/keeplo/platform/KeeploPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.keeplo.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeploPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeeploPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/keeplo/platform/KeeploPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.keeplo.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeeploPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
