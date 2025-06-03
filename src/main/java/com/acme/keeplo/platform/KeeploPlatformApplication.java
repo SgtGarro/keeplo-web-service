6e4dad164b94a8166c7417703def247ceff986c0
yigabriel <gabrielsacc7@gmail.com>
2025-06-02 21:41:31 -0500

    chore: initial commit

--- src/main/java/com/acme/keeplo/platform/KeeploPlatformApplication.java
+++ src/main/java/com/acme/keeplo/platform/KeeploPlatformApplication.java
@@ -2,7 +2,21 @@ package com.acme.keeplo.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+/**
+ * KeeploPlatformApplication
+ *
+ * @summary
+ * The main class of the Keeplo Platform application.
+ * It is responsible for starting the Spring Boot application.
+ * It also enables JPA auditing.
+ *
+ * @since 1.0
+ */
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class KeeploPlatformApplication {
 
